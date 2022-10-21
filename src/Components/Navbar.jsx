f58ae75496be4365d7c1ad90437d6fc1f69d4ad1
Jamgoori <lks2354@naver.com>
2022-10-21 17:33:46 +0900

    Navbar Css적용

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div>Navbar</div>
-  )
+    <Header>
+        <LeftHome>
+      <Link to="/">
+        <img src="../images/Logo.png" />
+      </Link>
+      </LeftHome>
+      <Nav>
+
+          <ul>
+            <li>
+             <a><Link to="book">Home</Link></a>
+            </li>
+            <li>
+            <a><Link to="about">about</Link></a>
+            </li>
+          </ul>
+      </Nav>
+    </Header>
+  );
 }
 
-export default Navbar
+const Header = styled.header`
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: 1fr;
+  align-items: center;
+`;
+const LeftHome = styled.h1`
+  display: inline-block;
+  vertical-align: middle;
+  color: white;
+  margin: 0.5rem;
+  padding: 0;
+`
+const Nav = styled.nav`
+  display: inline-block;
+  vertical-align: middle;
+  ul {
+    list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  }
+  li{
+    display: block;
+  text-align: center;
+  margin: .25rem;
+  padding: .5rem 1rem;
+  flex-basis: 25%;
+  }
+  a{  display: block;
+  text-align: center;
+  margin: .25rem;
+  padding: .5rem 1rem;
+  font-size: 1.5rem;
+  }
+  a:hover{
+    color: #b83ab8
+  }
+`;
+
+export default Navbar;
